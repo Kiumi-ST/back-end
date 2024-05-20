d9fabc21bb677d8a72932445d0a254203d17462c
soyoon1 <20210841@duksung.ac.kr>
2024-05-20 22:43:40 +0900

    [생성] Spring boot 프로젝트 생성 (2024.05.20)

---
+++ src/test/java/st/kiumi/KiumiApplicationTests.java
@@ -0,0 +1,13 @@
+package st.kiumi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiumiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
