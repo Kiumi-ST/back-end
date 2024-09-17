5a2764a8fb3cf71a1de545dfcddf6099b1a40501
sunflwwer <ghosti007@naver.com>
2024-09-18 00:01:48 +0900

    [추가] 백엔드 배포, HomeController 추가 (2024.09.18)

---
+++ src/main/java/st/kiumi/controller/HomeController.java
@@ -0,0 +1,13 @@
+package st.kiumi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to Kiumi Application!";
+    }
+}
